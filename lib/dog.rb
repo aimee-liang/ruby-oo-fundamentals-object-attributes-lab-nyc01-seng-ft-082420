3e02aacb859e6c59439e5c15cfe2874b6573b94a
Aimee Liang <aimeeeliang@gmail.com>
2020-08-25 14:07:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,14 @@
 class Dog 
   
-def name (name)
-  @name = name
-end
+  def name (name)
+    @name = name
+  end
 
-def name=(new_name)
-  @name = new_name
-end
+  def name=(new_name)
+    @name = new_name
+  end
 
+end
   
 Clifford = Dog.new 
 Clifford.name
